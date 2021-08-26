f1ab868580db8a87b0a4c9a617aa398bb74fcf3b
reham90 <rehamadel344@gmail.com>
2021-08-26 13:00:41 +0300

    edit

--- js/my-js.js
+++ js/my-js.js
@@ -2,73 +2,56 @@
   $(function() { // DOM ready
     // If a link has a dropdown, add sub menu toggle.
     $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
+      $(this).siblings('.one').slideToggle();
       // Close one dropdown when selecting another
-      $('.nav-dropdown').not($(this).siblings()).hide();
+      $('.one').not($(this).siblings()).hide();
       e.stopPropagation();
     });
+
+     // If a link has a dropdown, add sub menu toggle.
+    $('.sec-list a').hover(function(e) {
+      $(this).siblings('.two').css("display" ,"block");
+      // Close one dropdown when selecting another
+     
+      e.stopPropagation();
+    });
+
+$('.two').hover(function(e) {
+      $(".two").css("display" ,"block");
+      // Close one dropdown when selecting another
+     
+      e.stopPropagation();
+    });
+
     // Clicking away from dropdown will remove the dropdown class
     $('html').click(function() {
       $('.nav-dropdown').hide();
     });
     // Toggle open and close nav styles on click
     $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
+      $('nav ul').toggle("slide");
+
+var y = document.getElementById("re-show");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+
+
+   var z = document.getElementById("re-hide");
+  if (z.style.display === "block") {
+    z.style.display = "none";
+  } else {
+    z.style.display = "block";
+  }
+
+
+
     });
     // Hamburger to X toggle
     $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
+      this.slideToggle('300');
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
-
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= 50) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-$('.carousel').carousel();
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
- new WOW().init();
-
- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
- var btn = $('#myBtn');
-
-$(window).scroll(function() {
-
-});
-
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-var atr = 0;
-    $('.scroll-down').click(function () {
-        atr = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(atr).offset().top
-        }, 1000);
-
-        return false;
-    });
